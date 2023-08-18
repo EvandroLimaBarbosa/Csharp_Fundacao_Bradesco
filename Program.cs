3ebbc741e5434bb035b4e39a4e05ad51cd00d545
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2023-08-18 07:20:15 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace OlaMundo
+{
+    class Program
+    {
+         static void Main()
+        {
+            var idade = 25;
+            var salario = 1285.76;
+
+            Console.WriteLine("Idade {0}, Salário {1}", idade, salario);
+        }
+    }
+}
