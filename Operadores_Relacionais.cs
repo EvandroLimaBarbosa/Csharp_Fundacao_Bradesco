46a3d8535a3c5dd7587a32762275d0ac8baad942
Evandro Lima <96010876+EvandroLimaBarbosa@users.noreply.github.com>
2023-08-18 07:40:39 -0300

    Create Operadores_Relacionais.cs

---
+++ Operadores_Relacionais.cs
@@ -0,0 +1,14 @@
+﻿namespace operadoresRelacionais
+{
+    class Program
+    {
+        static void Main()
+        {
+            double nota1 = 8.0;
+            double nota2 = 9.0;
+
+            Console.WriteLine("Nota maior que 7? {0}", nota1 > 7.0);
+            Console.WriteLine("Nota maior que 7? {0}", nota2 == 10.0);
+        }
+    }
+}
